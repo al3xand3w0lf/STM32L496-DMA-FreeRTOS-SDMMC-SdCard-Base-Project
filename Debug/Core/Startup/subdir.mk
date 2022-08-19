c130d6a383e0e0b14143a044d807e76487fcc6f7
wolf <alexander.wolf@gmx.ch>
2022-08-19 17:18:43 +0200

    update

--- Debug/Core/Startup/subdir.mk
+++ Debug/Core/Startup/subdir.mk
@@ -16,7 +16,7 @@ S_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I"D:/Documents/PROJECTS/101 HELIOS/3 Programmieren STM32/Firmware/HELIOS_STM32/Middlewares/Third_Party/SEGGER/SEGGER" -I"D:/Documents/PROJECTS/101 HELIOS/3 Programmieren STM32/Firmware/HELIOS_STM32/Middlewares/Third_Party/SEGGER/SEGGER/Config" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I"D:/Documents/PROJECTS/101 HELIOS/3 Programmieren STM32/Firmware/HELIOS_STM32_github/Middlewares/Third_Party/SEGGER/SEGGER" -I"D:/Documents/PROJECTS/101 HELIOS/3 Programmieren STM32/Firmware/HELIOS_STM32_github/Middlewares/Third_Party/SEGGER/SEGGER/Config" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
 
 clean: clean-Core-2f-Startup
 
--- Debug/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.d
+++ Debug/Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.d
@@ -1,6 +1,6 @@
 Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.o: \
  ../Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.S \
  ../Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_RTT.h \
- D:/Documents/PROJECTS/101\ HELIOS/3\ Programmieren\ STM32/Firmware/HELIOS_STM32/Middlewares/Third_Party/SEGGER/SEGGER/Config/SEGGER_RTT_Conf.h
+ D:/Documents/PROJECTS/101\ HELIOS/3\ Programmieren\ STM32/Firmware/HELIOS_STM32_github/Middlewares/Third_Party/SEGGER/SEGGER/Config/SEGGER_RTT_Conf.h
 ../Middlewares/Third_Party/SEGGER/SEGGER/SEGGER_RTT.h:
-D:/Documents/PROJECTS/101\ HELIOS/3\ Programmieren\ STM32/Firmware/HELIOS_STM32/Middlewares/Third_Party/SEGGER/SEGGER/Config/SEGGER_RTT_Conf.h:
+D:/Documents/PROJECTS/101\ HELIOS/3\ Programmieren\ STM32/Firmware/HELIOS_STM32_github/Middlewares/Third_Party/SEGGER/SEGGER/Config/SEGGER_RTT_Conf.h:
